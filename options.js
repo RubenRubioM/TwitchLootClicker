72fb3e2a4315087479c90127f0c75a547163192d
Ruben <rubensipala@gmail.com>
2020-06-10 21:06:13 +0200

    v4.2 - Sort by time left

--- options.js
+++ options.js
@@ -37,8 +37,14 @@ function AddChannelPoints(){
             var sortedRanking = result['ChannelsPoints'].sort(function(a,b){
                 if(a.points<b.points){
                     return 1;
-                }else{
+                }else if(a.points>b.points){
                     return -1;
+                }else{
+                    if(a.timeToBox>b.timeToBox){
+                        return 1;
+                    }else{
+                        return -1
+                    }
                 }
             });
 
@@ -59,8 +65,8 @@ function AddChannelPoints(){
                                         <td> ${element.points} </td>
                                         <td> ${ millisToMinutesAndSeconds(element.timeToBox)} </td>
                                     </tr>`;
+
                 labels.push(res);
-                
                 values.push(element.points);
             });
             
@@ -110,6 +116,48 @@ function StorageLocalInSync(){
         chrome.storage.sync.set({'ChannelsPoints': result['ChannelsPoints']}, function() {
             //console.log('El nuevo valor de  ' +channel.name+ ' es ahora ' + channel.points);
         });
+    });
+}
+
+function DeleteChannel(){
+    console.log("asd");
+}
+
+function DEBUG_ADD_POINTS(channel){
+    var url = channel;
+
+    chrome.storage.local.get(['ChannelsPoints'], function(result){
+        var channel = {name: url, points: 50, timeToBox: 894000};
+        if(isEmpty(result)){
+            var channelArray = [];
+            channelArray.push(channel);
+            chrome.storage.local.set({'ChannelsPoints': channelArray}, function() {
+                //console.log('El nuevo valor de  ' +channel.name+ ' es ahora ' + channel.points);
+            });
+        }else{
+
+            //To check if this channel already exists
+            var found = false;
+            result['ChannelsPoints'].forEach(function(element, i, array){
+                if(element.name == url){
+                    //We already have stored this channel
+                    element.points += 50; //TODO: Change this static 50 for 100 if he is suscribed
+                    element.timeToBox = 894000;
+                    //console.log(element.points);
+                    found = true;
+                }
+            });
+            
+            //If the channel identified is not in the storage, we create a new object
+            if(!found){
+                result['ChannelsPoints'].push(channel);
+            }
+
+            chrome.storage.local.set({'ChannelsPoints': result['ChannelsPoints']}, function() {
+                //console.log('El nuevo valor de  ' +channel.name+ ' es ahora ' + channel.points);
+            });
+
+        }
     });
 }
     
