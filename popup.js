3a38be4f9cb9df2e73063816d066ee0a7d47b86e
Ruben Rubio Martinez <rubensipala@gmail.com>
2021-10-17 10:12:33 +0200

    v3.1.0 - Stats button on pop up

--- popup.js
+++ popup.js
@@ -1,28 +1,33 @@
-function isEmpty(obj) {
-  for(var key in obj) {
-      if(obj.hasOwnProperty(key))
-          return false;
-  }
-  return true;
-}
+// function isEmpty(obj) {
+//   for(var key in obj) {
+//       if(obj.hasOwnProperty(key))
+//           return false;
+//   }
+//   return true;
+// }
 
-chrome.runtime.onMessage.addListener(function(response,sender,sendResponse){
-    console.log(response);
+document.addEventListener('DOMContentLoaded', function() {
+    var goToStatsButton = document.getElementById('goToStats');
+
+    goToStatsButton.addEventListener('click', function() {
+        window.open("chrome-extension://" + chrome.runtime.id + "/options.html", '_blank').focus(); 
+    });
 });
 
-function UpdateTimeLeft(){
-    var div = document.getElementById('time-to-box');
-    var url = window.location.href;
-    chrome.storage.sync.get(['ChannelsPoints'], function(result){
-        if(!isEmpty(result)){
+// function UpdateTimeLeft(){
+//     console.log("ENTRA POPUP");
+//     var div = document.getElementById('time-to-box');
+//     var url = window.location.href;
+//     chrome.storage.sync.get(['ChannelsPoints'], function(result){
+//         if(!isEmpty(result)){
 
-            result['ChannelsPoints'].forEach(function(element, i, array){
-                if(element.name == url){
-                    div.innerText = element.timeToBox;
-                }
-            });
-        }
-    });
-}
+//             result['ChannelsPoints'].forEach(function(element, i, array){
+//                 if(element.name == url){
+//                     div.innerText = element.timeToBox;
+//                 }
+//             });
+//         }
+//     });
+// }
 
-setInterval(UpdateTimeLeft,1000);
+// setInterval(UpdateTimeLeft,1000);
